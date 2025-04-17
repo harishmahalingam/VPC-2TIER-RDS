390c162b803139f86c295d1cb380119329b23860
harishmahalingam <129191989+harishmahalingam@users.noreply.github.com>
2025-04-17 11:44:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ module "ec2" {
   source = "./modules/ec2"
   vpc_id = module.vpc.vpc_id
   subnet_id = module.vpc.public_subnet_id
-  key_name = var.key_name
+  key_name = "test-keypair"
 }
  
 module "rds" {
