cfe81b139e796a81b5f3f798af9ad367db62da99
harishmahalingam <129191989+harishmahalingam@users.noreply.github.com>
2025-04-17 15:29:54 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,18 +1,39 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
-
+resource "aws_vpc" "main" {
+cidr_block = "10.0.0.0/16"
+}
+ 
+resource "aws_subnet" "public" {
+vpc_id = aws_vpc.main.id
+cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+}
+ 
+resource "aws_subnet" "private" {
+  count                   = 2
+vpc_id = aws_vpc.main.id
+cidr_block = cidrsubnet("10.0.0.0/16", 8, count.index + 2)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = false
   tags = {
-    Terraform = "true"
-    Environment = "dev"
+    Name = "private-subnet-${count.index + 1}"
   }
+}
+ 
+data "aws_availability_zones" "available" {}
+ 
+resource "aws_internet_gateway" "gw" {
+vpc_id = aws_vpc.main.id
+}
+ 
+output "vpc_id" {
+value = aws_vpc.main.id
+}
+ 
+output "public_subnet_id" {
+value = aws_subnet.public.id
+}
+ 
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
 }
