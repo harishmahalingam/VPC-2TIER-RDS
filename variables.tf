ee052e746754fbe216546eb33ca8d637daab2412
harishmahalingam <129191989+harishmahalingam@users.noreply.github.com>
2025-04-17 15:31:39 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,12 @@
 
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+ 
+variable "key_name" {
+  description = "Name of the existing EC2 Key Pair to enable SSH access"
+  type        = string
+  default     = "test-keypair"
+}
