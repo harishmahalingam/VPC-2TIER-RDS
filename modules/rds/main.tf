9690885956b2d8d56feee24a5803a77b551e184a
harishmahalingam <129191989+harishmahalingam@users.noreply.github.com>
2025-04-17 15:26:52 +0530

    Update main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,73 +1,27 @@
-module "db" {
-  source = "terraform-aws-modules/rds/aws"
+variable "vpc_id" {
+  description = "ID of the VPC where the EC2 instance will be deployed"
+  type        = string
+}
+ 
+variable "subnet_id" {
+  description = "ID of the public subnet for the EC2 instance"
+  type        = string
+}
+ 
+variable "key_name" {
+  description = "Name of the existing EC2 Key Pair to enable SSH access"
+  type        = string
+  default     = "test-keypair"
+}
 
-  identifier = "demodb"
+variable "instance_type" {
+  description = "Name of the existing EC2 Key Pair to enable SSH access"
+  type        = string
+  default     = "t2.micro"
+}
 
-  engine            = "mysql"
-  engine_version    = "5.7"
-  instance_class    = "db.t3a.large"
-  allocated_storage = 5
-
-  db_name  = "demodb"
-  username = "user"
-  port     = "3306"
-
-  iam_database_authentication_enabled = true
-
-  vpc_security_group_ids = ["sg-12345678"]
-
-  maintenance_window = "Mon:00:00-Mon:03:00"
-  backup_window      = "03:00-06:00"
-
-  # Enhanced Monitoring - see example for details on how to create the role
-  # by yourself, in case you don't want to create it automatically
-  monitoring_interval    = "30"
-  monitoring_role_name   = "MyRDSMonitoringRole"
-  create_monitoring_role = true
-
-  tags = {
-    Owner       = "user"
-    Environment = "dev"
-  }
-
-  # DB subnet group
-  create_db_subnet_group = true
-  subnet_ids             = ["subnet-12345678", "subnet-87654321"]
-
-  # DB parameter group
-  family = "mysql5.7"
-
-  # DB option group
-  major_engine_version = "5.7"
-
-  # Database Deletion Protection
-  deletion_protection = true
-
-  parameters = [
-    {
-      name  = "character_set_client"
-      value = "utf8mb4"
-    },
-    {
-      name  = "character_set_server"
-      value = "utf8mb4"
-    }
-  ]
-
-  options = [
-    {
-      option_name = "MARIADB_AUDIT_PLUGIN"
-
-      option_settings = [
-        {
-          name  = "SERVER_AUDIT_EVENTS"
-          value = "CONNECT"
-        },
-        {
-          name  = "SERVER_AUDIT_FILE_ROTATIONS"
-          value = "37"
-        },
-      ]
-    },
-  ]
+variable "ami_id" {
+  description = "ID of the public subnet for the EC2 instance"
+  type        = string
+  default     = "ami-084568db4383264d4"
 }
