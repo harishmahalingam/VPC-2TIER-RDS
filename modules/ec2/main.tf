99bffff6991a6ea08ae26232f30f650d022b38c8
harishmahalingam <129191989+harishmahalingam@users.noreply.github.com>
2025-04-17 15:25:15 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,15 +1,32 @@
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-
-  name = "web-instance"
-
-  instance_type          = "t2.micro"
-  key_name               = "user1"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
-
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-sg"
+  description = "Security group for EC2 instance"
+  vpc_id      = var.vpc_id
+ 
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Replace with your IP for security
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+ 
+resource "aws_instance" "web" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = var.subnet_id
+vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  key_name               = var.key_name
+user_data = file("${path.module}/../../user_data/apache2.sh")
   tags = {
-name = "EC2-WEBSERVER"
+    Name = "WebServer"
   }
 }
