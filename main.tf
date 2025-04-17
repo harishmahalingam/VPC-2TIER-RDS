20e02730697aacdef10288e091380052e0c3a9c5
harishmahalingam <129191989+harishmahalingam@users.noreply.github.com>
2025-04-17 11:19:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
-
+provider "aws" {
+  region = "us-east-1"
+}
+ 
+module "vpc" {
+  source = "./modules/vpc"
+  vpc_name = "my-vpc-rds"
+}
+ 
+module "ec2" {
+  source = "./modules/ec2"
+  vpc_id = module.vpc.vpc_id
+  subnet_id = module.vpc.public_subnet_id
+  key_name = var.key_name
+}
+ 
+module "rds" {
+  source = "./modules/rds"
+  vpc_id = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnet_ids
+}
