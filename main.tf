043ad9b575e24ea88e604af54c8d81c9f71d6526
harishmahalingam <129191989+harishmahalingam@users.noreply.github.com>
2025-04-17 15:31:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
  
 module "vpc" {
   source = "./modules/vpc"
-  vpc_name = "my-vpc-rds"
+  vpc_name = "my-vpc"
 }
  
 module "ec2" {
   source = "./modules/ec2"
   vpc_id = module.vpc.vpc_id
   subnet_id = module.vpc.public_subnet_id
-  key_name = "test-keypair"
+  key_name = var.key_name
 }
  
 module "rds" {
